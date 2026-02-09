d772a321ee5ddd96d1a99ce9c1a7496ea0b6bc33
ollieadams23 <olliadams511@gmail.com>
2026-02-09 17:28:40 +1300

    sample quotes created

---
+++ codewisdom.js
@@ -0,0 +1,11 @@
+// 1. Store inspirational programming quotes
+
+// 2. Store best practices/tips
+
+// 3. Store programming wisdom
+
+// 4. Create function to get random item from array
+
+// 5. Generate random message from all three arrays
+
+// 6. Display the message
