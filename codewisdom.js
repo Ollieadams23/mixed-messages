1a28f209f4b15aaad8c13f3e9cdef1a71db920b9
ollieadams23 <olliadams511@gmail.com>
2026-02-09 18:03:02 +1300

    random logic created

--- codewisdom.js
+++ codewisdom.js
@@ -9,3 +9,19 @@
 // 5. Generate random message from all three arrays
 
 // 6. Display the message
+
+const quotes = require('./quotes.json');
+const practices = require('./bestpractices.json');
+const wisdoms = require('./programmingwisdoms.json'); 
+
+function getRandom(){
+// Generate random indices for each array
+    const quote = (Math.floor(Math.random()* quotes.quotes.length))
+    const practice = (Math.floor(Math.random()* practices.best_practices_quotes.length))
+    const wisdom = (Math.floor(Math.random()* wisdoms.wisdom.length))
+
+    const message = quotes.quotes[quote] + "\n\n" + practices.best_practices_quotes[practice] + "\n\n" + wisdoms.wisdom[wisdom];
+    console.log(message);
+}
+
+getRandom();
